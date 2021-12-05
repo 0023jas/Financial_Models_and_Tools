cfe94eb84970e6554bcdcb8cb028923abc65e84c
Araphor0 <77243283+Araphor0@users.noreply.github.com>
2021-12-05 19:58:44 +0000

    Uploading the template

---
+++ burn_bot/README.md
@@ -0,0 +1,17 @@
+# Burn_bot
+
+Burn_Bot is a python template for building twitter bots
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install tweepy.
+
+```bash
+pip3 install tweepy
+```
+
+or you could use the requirements.txt
+
+```bash
+pip3 install -r requirements.txt
+```
---
+++ burn_bot/bots/config.py
@@ -0,0 +1,31 @@
+##imports
+import tweepy
+import logging
+import os
+
+##Returns a logger with a specified name
+logger = logging.getLogger()
+
+def create_api():
+    '''
+    This function reads the authentication creadentials
+    from the environmental variables and creates the
+    Tweepy API object.
+    '''
+    ##The following environment variables go here
+    consumer_key = os.getenv("CONSUMER_KEY")
+    consumer_secret = os.getenv("CONSUMER_SECRET")
+    access_token = os.getenv("ACCESS_TOKEN")
+    access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True, 
+        wait_on_rate_limit_notify=True)
+    try:
+        api.verify_credentials()
+    except Exception as e:
+        logger.error("Error creating API", exc_info=True)
+        raise e
+    logger.info("API created")
+    return api
